d6244a17bb1cef5d5a5a20f4c8775e93fbb4462a
Rimpi Rani Baruah <73873611+Rimpi246@users.noreply.github.com>
2021-08-24 14:28:40 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,1 +1,28 @@
+// Worst case time complexity-O(n^2)
+// Best case time complexity-O(n)
+#include <bits/stdc++.h>
+using namespace std;
+void bubbleSort(int arr[], int n){
+    int flag;
+    for(int i=0; i<n-1; i++){
+        flag=0;
+        for(int j=0; j<n-1-i; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                flag=1;
+            }
+        }
+        if(flag==0)break;
+    }
+}
+int main()
+{
+    int arr[]={1,2,4};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
 
