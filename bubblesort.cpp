e9329a699da0395bfda0843dc064322ba3d90f7f
Rimpi Rani Baruah <73873611+Rimpi246@users.noreply.github.com>
2021-08-25 14:38:33 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -18,7 +18,7 @@ void bubbleSort(int arr[], int n){
 }
 int main()
 {
-    int arr[]={1,2,4};
+    int arr[]={3,5,27,8};
     int n=sizeof(arr)/sizeof(arr[0]);
     bubbleSort(arr, n);
     for(int i=0; i<n; i++){
